abd616ecec7be5e5868c2af8b8e37ccac00e26eb
Micah Stalberg <stalberm@oregonstate.edu>
2023-03-17 13:36:08 -0700

    Adds subpages text

---
+++ src/pages/Conversations.js
@@ -0,0 +1,27 @@
+function Conversations() {
+    
+    return (
+        <div>
+            <h1>Conversations</h1>
+            <p>
+                SAVANTS IN CONVERSATION
+                <br />
+                From intermediate to advanced, at SAVANTS, we offer conversation courses in
+                almost any language to students of all ages almost anywhere on Earth. As much
+                as we value online communications, we make every effort to offer such courses
+                in a person-to-person mode.
+        
+                What makes our conversation courses unique and different, besides the
+                incomparable quality of our tutors, is the opportunity to move beyond confident
+                chit-chat to fully competency in reading and writing as well. Even when kept
+                light, such skills constitute an important part of our courses. The overall
+                confidence gained by our students leads to increased competencies in language
+                fundamentals, which leads organically to more sophisticated and nuanced
+                communicative techniques and tactics.
+            </p>
+        </div>
+        
+    )
+}
+
+export default Conversations
---
+++ src/pages/Worshop.js
@@ -0,0 +1,25 @@
+function Workshop() {
+    
+    return (
+        <div>
+            <h1>Workshop</h1>
+            <p>
+                SAVANTS WRITING WORKSHOPS
+                <br />
+                Young, ambitious writers as well as seasoned professionals seek us out to deepen
+                and develop their skills. Our SAVANTS WRITING WORKSHOPS provide
+                high-quality feedback from accomplished authors. Whether you want to hone your
+                professional writing skills or produce a manuscript of original poetry, our writing
+                workshops offer all participants a challenging and rewarding experience.
+
+                Our advanced seminars are typically taken by junior professionals who want to
+                consult with senior SAVANTS tutors who are experts in these fields. As part of
+                our classic SAVANTS programs, we offer these courses face-to-face in as many
+                communities as we can.
+            </p>
+        </div>
+        
+    )
+}
+
+export default Workshop
