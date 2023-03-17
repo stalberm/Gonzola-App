d5391679a1bf946623ee11a78c60ed9973bc295d
stalberm <77121289+stalberm@users.noreply.github.com>
2023-03-17 14:58:23 -0700

    feat(pages): add basic text from slides to home page and subpages (#7)
    * Adds basic text from slides to home page and subpages
    * Adds subpages text
    * Resolves #3

---
+++ src/pages/Classic.js
@@ -0,0 +1,21 @@
+function Classic() {
+    
+    return (
+        <div>
+            <h1>Classic Classes</h1>
+            <p>
+                SAVANTS CLASSIC COURSES
+                <br />
+                At SAVANTS, we stay true to our roots in the Arts and Humanities, and we pride
+                ourselves in offering courses of incomparable quality in these areas. Our master
+                classes in cinema, languages and literatures constitute our core, classic courses;
+                they are the foundation of our offerings, and solidly establish our claim to
+                excellence. We strive to make our core classic courses available in person-
+                to-person format at as many locations as we can.
+            </p>
+        </div>
+        
+    )
+}
+
+export default Classic
