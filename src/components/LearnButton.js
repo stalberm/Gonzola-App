30f7656dc3fa835e1a2ad063029325719736b948
stalberm <77121289+stalberm@users.noreply.github.com>
2023-03-18 15:56:56 -0700

    feat: add button component for navigating to subpages (#10)

---
+++ src/components/LearnButton.js
@@ -0,0 +1,16 @@
+import { NavLink } from 'react-router-dom'
+import './LearnButton.css'
+
+function LearnButton(props) {
+    const {text = "LEARN MORE"} = props
+    return (
+    <div className="learn-more">
+        <img src="images/hand.jpg" alt="A Hand Pointing Right"/>
+        <NavLink to={props.path} className="link">
+            {text}
+        </NavLink>
+    </div>
+    )
+}
+
+export default LearnButton
