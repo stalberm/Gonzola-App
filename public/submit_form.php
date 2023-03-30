e88600581312baec7be94e6e1815bafb5cec6494
theusaf <32113157+theusaf@users.noreply.github.com>
2023-03-30 08:51:09 -0700

    feat: add php form

--- public/submit_form.php
+++ public/submit_form.php
@@ -1,2 +1,72 @@
 <?php
+
+$errors = [];
+
+// Verification of data
+$required_attributes = [
+  "first_name",
+  "address",
+  "city",
+  "state",
+  "zip",
+  "country",
+  "email",
+  "tutoring_nature",
+  "tutoring_type",
+  "service_reception",
+  "subject"
+];
+
+foreach ($required_attributes as $attribute) {
+  if (!isset($_POST[$attribute]) || empty($_POST[$attribute])) {
+    $errors[] = "The field $attribute is required.";
+  }
+}
+
+
+// Set default values
+$default_values = [
+  "last_name" => "",
+  "phone" => "",
+  "hours" => "",
+  "info_physical" => FALSE,
+  "info_virtual" => FALSE,
+];
+
+foreach ($default_values as $attribute => $value) {
+  if (!isset($_POST[$attribute])) {
+    $_POST[$attribute] = $value;
+  }
+}
+
+// If there are errors, return to the form
+// Put errors in the header as a JSON array
+if (!empty($errors)) {
+  header("FormErrors: " . json_encode($errors));
+  header("Location: /form");
+  http_response_code(400);
+  exit;
+}
+
+$name = trim($_POST["first_name"] . " " . $_POST["last_name"]);
+
+// Send email
+$to = getenv("EMAIL_TO");
+$subject = "[savants.network] Tutoring request from $name";
+$address = "{$_POST['address']} {$_POST['city']} {$_POST['state']} {$_POST['zip']}, {$_POST['country']}";
+$message = "
+Contact Information:
+Name: $name
+Address: $address
+Email: {$_POST['email']}
+Phone: {$_POST['phone']}
+
+Tutoring Information:
+Nature: {$_POST['tutoring_nature']}
+Type: {$_POST['tutoring_type']}
+Reception Preference: {$_POST['service_reception']}
+Subject Interests: {$_POST['subject']}
+Hours: {$_POST['hours']}
+" . $_POST['info_physical'] ? 'I would like to receive information regarding all courses offered in my town, city, or region\n': ''
+  . $_POST['info_virtual'] ? 'I would like to receive information regarding all courses offered virtually\n': ''
 ?>
--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,78 +1,224 @@
-import "./Form.css"
+import "./Form.css";
 
 function Form() {
-
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    console.log(e.target.elements.first.value)
-  }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = new FormData(e.target);
+    const response = await fetch("/submit_form.php", {
+      method: "POST",
+      body: data,
+    });
+    if (response.ok) {
+      // TODO: go back to home page
+    } else {
+      // TODO: show errors
+      const errors = JSON.parse(response.headers.get("FormErrors"));
+      console.log(errors);
+    }
+  };
 
   return (
     <div className="form-container">
       <form onSubmit={handleSubmit}>
         <div>
-          First Name: <input type="text" placeholder="First Name" name="first_name" className="text-input" />
-          Last Name: <input type="text" placeholder="Last Name" name="last_name" className="text-input" />
+          First Name:{" "}
+          <input
+            type="text"
+            placeholder="First Name"
+            name="first_name"
+            className="text-input"
+            required
+          />
+          Last Name:{" "}
+          <input
+            type="text"
+            placeholder="Last Name"
+            name="last_name"
+            className="text-input"
+          />
         </div>
         <div>
-          Address: <input type="text" placeholder="Address" name="address" className="text-input" />
-          City: <input type="text" placeholder="City" name="city" className="text-input" />
-          State/Province: <input type="text" placeholder="State" name="state" className="text-input" />
-          Zip: <input type="text" placeholder="Zip" name="zip" className="text-input"/>
-          Country: <input type="text" placeholder="Country" name="country" className="text-input" />
+          Address:{" "}
+          <input
+            type="text"
+            placeholder="Address"
+            name="address"
+            className="text-input"
+            required
+          />
+          City:{" "}
+          <input
+            type="text"
+            placeholder="City"
+            name="city"
+            className="text-input"
+            required
+          />
+          State/Province:{" "}
+          <input
+            type="text"
+            placeholder="State"
+            name="state"
+            className="text-input"
+            required
+          />
+          Zip:{" "}
+          <input
+            type="text"
+            placeholder="Zip"
+            name="zip"
+            className="text-input"
+            pattern="[0-9]+"
+            required
+          />
+          Country:{" "}
+          <input
+            type="text"
+            placeholder="Country"
+            name="country"
+            className="text-input"
+          />
         </div>
         <div>
-          Phone: <input type="text" placeholder="Phone" name="phone" className="text-input" />
+          Phone:{" "}
+          <input
+            type="phone"
+            placeholder="Phone"
+            name="phone"
+            className="text-input"
+            pattern="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$"
+          />
         </div>
         <div>
-          Email: <input type="text" placeholder="Email" name="email" className="text-input" />
+          Email:{" "}
+          <input
+            type="text"
+            placeholder="Email"
+            name="email"
+            className="text-input"
+            required
+          />
         </div>
         <div>
-          What is the nature of the tutoring services you require?
-          Private <input type="radio" name="tutoring_nature" />
-          Professional <input type="radio" name="tutoring_nature" />
+          What is the nature of the tutoring services you require? Private{" "}
+          <input type="radio" name="tutoring_nature" value="Private" required />
+          Professional{" "}
+          <input
+            type="radio"
+            name="tutoring_nature"
+            value="Professional"
+            required
+          />
         </div>
         <div>
           In which type of our tutoring services are you interested?
           <div>
-            One-on-One <input type="radio" name="tutoring_type" />
-            Small Group <input type="radio" name="tutoring_type" />
-            Cultural Consulting <input type="radio" name="tutoring_type" />
-            Professional Seminars <input type="radio" name="tutoring_type" />
-            Conversations <input type="radio" name="tutoring_type" />
-            Book and Film Club <input type="radio" name="tutoring_type" />
-            Workshops <input type="radio" name="tutoring_type" />
+            One-on-One{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="One-on-One"
+              required
+            />
+            Small Group{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Small Group"
+              required
+            />
+            Cultural Consulting{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Culture Consulting"
+              required
+            />
+            Professional Seminars{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Professional Seminars"
+              required
+            />
+            Conversations{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Conversations"
+              required
+            />
+            Book and Film Club{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Book and Film Club"
+              required
+            />
+            Workshops{" "}
+            <input
+              type="radio"
+              name="tutoring_type"
+              value="Workshops"
+              required
+            />
           </div>
         </div>
         <div>
           How would you prefer to receive such services?
           <div>
-            Virtually <input type="radio" name="service_receiption" />
-            In Person <input type="radio" name="service_receiption" />
-            Mix <input type="radio" name="service_receiption" />
-            Doesn't Matter <input type="radio" name="service_receiption" />
+            Virtually{" "}
+            <input
+              type="radio"
+              value="Virtually"
+              name="service_reception"
+              required
+            />
+            In Person{" "}
+            <input
+              type="radio"
+              value="In Person"
+              name="service_receiption"
+              required
+            />
+            Mix{" "}
+            <input
+              type="radio"
+              value="Mix"
+              name="service_receiption"
+              required
+            />
+            Doesn't Matter{" "}
+            <input
+              type="radio"
+              value="Doesn't Matter"
+              name="service_receiption"
+              required
+            />
           </div>
         </div>
         <div>
           What subject or subjects are you interested in studying?
-          <input type="text" name="subject" className="text-input" />
+          <input type="text" name="subject" className="text-input" required />
         </div>
         <div>
           How many hours could you devote to studying for each meeting?
           <input type="number" name="hours" className="text-input" />
         </div>
         <div>
-          I would like to receive information regarding all courses offered in my town, city, or region
-          <input type="checkbox" name="info-physical" />
+          I would like to receive information regarding all courses offered in
+          my town, city, or region
+          <input type="checkbox" name="info_physical" />
         </div>
         <div>
-          I would like to receive information regarding all courses offered virtually
-          <input type="checkbox" name="info-virtual" />
+          I would like to receive information regarding all courses offered
+          virtually
+          <input type="checkbox" name="info_virtual" />
         </div>
         <input type="submit" value="Submit" />
       </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
