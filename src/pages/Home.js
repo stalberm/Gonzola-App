d129acd435f2793398fefa8f2952a3322c2aa4a2
theusaf <32113157+theusaf@users.noreply.github.com>
2023-03-30 11:10:38 -0700

    feat(Home): add bold to SAVANTS text

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -55,13 +55,13 @@ function Home() {
           path="/virtual"
           orientation="right"
         >
-          At SAVANTS we offer virtual custom courses in any conceivable subject
-          virtually. Whatever your need, whatever your curiosity, we are here to
-          provide a curated educational encounter. Across time zones and space,
-          our tutors and students meet online to explore a variety of learning
-          experiences. Our tutors create unique custom courses to students of
-          all ages and all levels, from basic to advanced, from all four corners
-          of the globe.
+          At <strong>SAVANTS</strong> we offer virtual custom courses in any
+          conceivable subject virtually. Whatever your need, whatever your
+          curiosity, we are here to provide a curated educational encounter.
+          Across time zones and space, our tutors and students meet online to
+          explore a variety of learning experiences. Our tutors create unique
+          custom courses to students of all ages and all levels, from basic to
+          advanced, from all four corners of the globe.
         </SubpageSection>
         <SubpageSection
           title="Professional Savants Seminars"
@@ -70,13 +70,13 @@ function Home() {
           path="/classic"
           orientation="left"
         >
-          At SAVANTS, we stay true to our roots in the Arts and Humanities, and
-          we pride ourselves in offering courses of incomparable quality in
-          these areas. Our master classes in cinema, languages and literatures
-          constitute our core, classic courses; they are the foundation of our
-          offerings, and solidly establish our claim to excellence. We strive to
-          make our core classic courses available in person- to-person format at
-          as many locations as we can.
+          At <strong>SAVANTS</strong>, we stay true to our roots in the Arts and
+          Humanities, and we pride ourselves in offering courses of incomparable
+          quality in these areas. Our master classes in cinema, languages and
+          literatures constitute our core, classic courses; they are the
+          foundation of our offerings, and solidly establish our claim to
+          excellence. We strive to make our core classic courses available in
+          person- to-person format at as many locations as we can.
         </SubpageSection>
         <SubpageSection
           title="Professional Savants Seminars"
@@ -85,15 +85,15 @@ function Home() {
           path="/seminars"
           orientation="right"
         >
-          The professional SAVANTS seminars are designed to provide actual,
-          accountable results and real value at a fast-paced rhythm. Our
-          professional students have excelled in their careers so far, but now
-          need to more fully master a particular subject (usually a foreign
-          language, but sometimes a specific aspect of business). Typically
-          sponsored by companies directly, the SAVANTS SEMINARS are always
-          custom built around the particular needs students or small groups.
-          These courses engage business-related issues and practices from the
-          very first meeting.
+          The professional <strong>SAVANTS</strong> seminars are designed to
+          provide actual, accountable results and real value at a fast-paced
+          rhythm. Our professional students have excelled in their careers so
+          far, but now need to more fully master a particular subject (usually a
+          foreign language, but sometimes a specific aspect of business).
+          Typically sponsored by companies directly, the
+          <strong>SAVANTS SEMINARS</strong> are always custom built around the
+          particular needs students or small groups. These courses engage
+          business-related issues and practices from the very first meeting.
         </SubpageSection>
         <SubpageSection
           title="Savants Cultural Consulting"
@@ -115,11 +115,10 @@ function Home() {
           path="/conversations"
           orientation="right"
         >
-          <strong>SAVANTS IN CONVERSATION</strong> from intermediate to
-          advanced, at SAVANTS, we offer conversation courses in almost any
-          language to students of all ages almost anywhere on Earth. As much as
-          we value online communications, we make every effort to offer such
-          courses in a person-to-person mode.
+          From intermediate to advanced, at <strong>SAVANTS</strong>, we offer
+          conversation courses in almost any language to students of all ages
+          almost anywhere on Earth. As much as we value online communications,
+          we make every effort to offer such courses in a person-to-person mode.
         </SubpageSection>
         <SubpageSection
           title="Savants Book and Film Clubs"
@@ -128,19 +127,19 @@ function Home() {
           path="/clubs"
           orientation="left"
         >
-          Our SAVANTS BOOK AND FILM CLUBS are one of our most popular learning
-          opportunities. Have you ever wanted an expert to walk you through the
-          nuances of French New Wave cinema or German Expressionism? Are you
-          looking for someone to help you curate a crash course in Classic
-          Hollywood? Curious about Charles Dickens or Jane Austen but don’t know
-          where to start? Or maybe you’re looking to brush up on classical
-          Chinese poetry or the best of current Young Adult fiction? Want to
-          debate The Book vs. The Movie Version of a story you love? We’ve got
-          you. Typically offered in a six-week format, in our book and film
-          clubs, tutor and student create a tailor-made program designed to meet
-          your interests and prior training. We frequently offer these
-          experiences for families or groups of friends looking to take their
-          next gathering to the next level.
+          Our <strong>SAVANTS</strong> BOOK AND FILM CLUBS are one of our most
+          popular learning opportunities. Have you ever wanted an expert to walk
+          you through the nuances of French New Wave cinema or German
+          Expressionism? Are you looking for someone to help you curate a crash
+          course in Classic Hollywood? Curious about Charles Dickens or Jane
+          Austen but don’t know where to start? Or maybe you’re looking to brush
+          up on classical Chinese poetry or the best of current Young Adult
+          fiction? Want to debate The Book vs. The Movie Version of a story you
+          love? We’ve got you. Typically offered in a six-week format, in our
+          book and film clubs, tutor and student create a tailor-made program
+          designed to meet your interests and prior training. We frequently
+          offer these experiences for families or groups of friends looking to
+          take their next gathering to the next level.
         </SubpageSection>
         <SubpageSection
           title="Savants Writing Workshops"
@@ -150,11 +149,11 @@ function Home() {
           orientation="right"
         >
           Young, ambitious writers as well as seasoned professionals seek us out
-          to deepen and develop their skills. Our SAVANTS WRITING WORKSHOPS
-          provide high-quality feedback from accomplished authors. Whether you
-          want to hone your professional writing skills or produce a manuscript
-          of original poetry, our writing workshops offer all participants a
-          challenging and rewarding experience.
+          to deepen and develop their skills. Our <strong>SAVANTS</strong>
+          WRITING WORKSHOPS provide high-quality feedback from accomplished
+          authors. Whether you want to hone your professional writing skills or
+          produce a manuscript of original poetry, our writing workshops offer
+          all participants a challenging and rewarding experience.
         </SubpageSection>
         <LearnButton
           path="/form"
