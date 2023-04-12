3de29b504c27c4402c3ae85caf48be24463c5162
theusaf <theusafyt@gmail.com>
2023-04-12 10:35:53 -0700

    feat(HomeLogo): add blurb
    - References #23

--- src/components/HomeLogo.jsx
+++ src/components/HomeLogo.jsx
@@ -4,6 +4,7 @@ export default function HomeLogo() {
   return (
     <div className="home-logo">
       <div>
+        <span className="welcome-blurb">Welcome to the</span>
         <div className="icon-container">
           <div className="left logo-half">
             <img src="/images/icon_half_left.svg" alt="left half of logo" />
