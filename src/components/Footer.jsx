20453ed326706d9a60719cab35e927d44462fcac
theusaf <theusafyt@gmail.com>
2023-05-07 14:44:16 -0700

    feat(Footer): remove unwanted text

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,6 @@ import "./Footer.css";
 export default function Footer() {
   return (
     <div id="footer">
-      <p>
-        By virtue of its extremely exclusive nature, SAVANTS NETWORK does NOT
-        publicize its services through any form of mass or social media.
-      </p>
       <p>&copy; 2023 SAVANTS NETWORK</p>
     </div>
   );
